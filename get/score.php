949de5ba62e1deb684edee4c053f55ad550bfe74
SiNesTroWarRoCk <warrock66678@gmail.com>
2018-11-12 14:36:25 +0700

    Update get/score.php

--- get/score.php
+++ get/score.php
@@ -2,28 +2,29 @@
 <html lang="en">
 
 <head>
-  <!-- Required meta tags -->
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Premier League</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
 </head>
 
 <body>
-  <table class="table">
+<div class="table-responsive">
+  <table class="table table-striped">
+	<caption>Premier League - Table</caption>
     <thead class="thead-dark">
-      <tr>
-	
-        <th scope="col">TEAM</th>
-        <th scope="col">P</th>
-        <th scope="col">W</th>
-        <th scope="col">D</th>
-        <th scope="col">L</th>
-        <th scope="col">Pts</th>
-      </tr>
+		<tr>
+			
+			<th><abbr title="Position">Pos</abbr></th>
+			<th>Team</th>
+			<th><abbr title="Played">Pld</abbr></th>
+			<th><abbr title="Won">W</abbr></th>
+			<th><abbr title="Drawn">D</abbr></th>
+			<th><abbr title="Lost">L</abbr></th>
+			<th><abbr title="Pts">Pts</abbr></th>
+		</tr>
     </thead>
     <tbody>
 		<?php
@@ -37,21 +38,33 @@
 			<?php foreach($json_arr['standings'][0]['table'] as $standings){?>
 			
 				<?php $position=$standings['position'];?>
-				<td><?php print ($position);?></td>
-			</tr>
-			<?php }?>
-			
-			<tr>
-			<?php
-			foreach($json_arr['standings'][0]['table'] as $standings) {?>
-			
+				<td><?php print ($position); ?></td> 
 				<?php $team=$standings['team']['name']; ?>
 				<td><?php print ($team)?></td>		
-			</tr>
+				<?php $play=$standings['playedGames']; ?>
+				<td><?php print ($play)?></td>	
+				<?php $won=$standings['won']; ?>
+				<td><?php print ($won)?></td>
+				<?php $draw=$standings['draw']; ?>
+				<td><?php print ($draw)?></td>		
+				<?php $lost=$standings['lost']; ?>
+				<td><?php print ($lost)?></td>
+				<?php $points=$standings['points']; ?>
+				<td><?php print ($points)?></td>		
+			</tr>		
 			<?php }?>
+			
+			
+
+		
+
+			
+			
+    
 			
     </tbody>
   </table>
+  </div>
   <!-- Optional JavaScript -->
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
